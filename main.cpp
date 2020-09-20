7eff38fc1d7ce13fef3e0e38eed403420b89d186
santiagopr123 <santiago.pereira@udea.edu.co>
2020-09-20 15:15:37 -0500

    bocetos casi terminados

--- main.cpp
+++ main.cpp
@@ -4,7 +4,14 @@ using namespace std;
 
 int potencia(int numero);
 int primos(int number);
-int multiplos(int num);
+int multiplos_del_dos(int num);
+int multiplos_del_tres(int num);
+int multiplos_del_cinco(int num);
+int multiplos_del_siete(int num);
+int potencia_distinto_numero(int nume, int poten);
+int divisores(int nu);
+
+
 
 
 int main()
@@ -84,6 +91,59 @@ int main()
     cout<<"el maximo factor primo es: "<<suma;
     */
 
+    /*
+    int a = 0,b = 0,c = 0, d = 0,max = 0,max1 = 0,max2 = 0,max3 = 0,result = 0,pri = 1;
+    int x =90;
+    for(int i= 1; i<=x; i++)
+    {
+        if(i>7 && primos(i)==0)
+        {
+            pri = pri * i;
+        }
+        a = multiplos_del_dos(i);
+        b = multiplos_del_tres(i);
+        c = multiplos_del_cinco(i);
+        d = multiplos_del_siete(i);
+
+        if(a >= max)
+        {
+            max = a;
+        }
+        if(b >= max1)
+        {
+            max1 = b;
+        }
+        if(c >= max2)
+        {
+            max2 = c;
+        }
+        if(d >= max3)
+        {
+            max3 = d;
+        }
+
+    }
+    result = potencia_distinto_numero(2,max) * potencia_distinto_numero(3,max1) * potencia_distinto_numero(5,max2) * potencia_distinto_numero(7,max3) * pri;
+    cout<<"el minimo comun multiplo es: "<< result;
+    */
+
+    /*
+    int suma = 1,verificador = 0, k = 6;
+
+    for(int i = 2;; i++)
+    {
+        suma = suma + i ;
+        verificador = divisores(suma);
+        if(verificador > k)
+        {
+            cout<<"el numero es: "<<suma<<" y tiene: "<<verificador<<" divisores.";
+            break;
+
+        }
+        cout<<"esta es la suma: "<<suma;
+    }
+
+    */
 
 
 
@@ -95,6 +155,11 @@ int main()
 int potencia(int numero)
 {
     int constante = numero,multiplicacion = numero;
+    if(numero == 0)
+    {
+        multiplicacion = 1;
+        return multiplicacion;
+    }
     for(int i=1; i<numero; ++i)
     {
         multiplicacion = multiplicacion * constante;
@@ -122,18 +187,89 @@ int primos(int number)
     }
 }
 
-int multiplos(int num)
+int multiplos_del_dos(int num)
 {
-    for(int i = 1; i<= num; i++)
+    int suma = 0;
+    while(num%2 == 0)
     {
 
+        num /= 2;
+        suma += 1;
+
+
     }
+    return suma;
 }
 
+int multiplos_del_tres(int num)
+{
+    int suma = 0;
+    while(num%3 == 0)
+    {
+
+        num /= 3;
+        suma += 1;
+
+    }
+    return suma;
+}
+
+int multiplos_del_cinco(int num)
+{
+    int suma = 0;
+    while(num%5 == 0)
+    {
+
+        num /= 5;
+        suma += 1;
 
 
+    }
+    return suma;
+}
+
+int multiplos_del_siete(int num)
+{
+    int suma = 0;
+    while(num%7 == 0)
+    {
+
+        num /= 7;
+        suma += 1;
 
 
+    }
+    return suma;
+}
 
+int potencia_distinto_numero(int nume, int poten)
+{
+    int constante = nume;
+    if(poten == 0)
+    {
+        nume = 1;
+        return nume;
+    }
+
+    for(int i= 1; i<poten; i++)
+    {
+        nume *= constante;
+    }
+    return nume;
+}
+
+int divisores(int nu)
+{
+    int  sumas = 0;
+    for(int i = 1; i<= nu; i++)
+    {
+        if(nu%i == 0)
+        {
+            sumas += 1;
+        }
+    }
+
+    return sumas;
+}
 
 
